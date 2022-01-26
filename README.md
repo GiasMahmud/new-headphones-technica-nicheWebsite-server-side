eda0ddacc68f2b59baded5ca6e48e4bfe092c60a
Gias Mahmud <mdgias181@gmail.com>
2022-01-27 02:30:53 +0600

    all commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BD Headphones Hub
+# New Headphones Technica
 
 This project live site: https://bd-headphones-hub.web.app/
 
@@ -18,6 +18,6 @@ for my fontawesome icon , creadits goes to them and for deploy creadit goes to n
 n this Modern age where technology is growing fast, our high technology based headphones with high defination could satisfy thousands of people . We are 3 times "Best Sound Tech" awarded. Throughout this website people can get their favorite headphones. We have very user friendly UI to fetch suitable products for every kind of users. Beside our reliable payment system demands the most safety gateway to users. User can have their account on there and explore his choice and count keep privacy of their own.
 Again, In every device this wensite is running cool and no unuseful  advertisement,  links, popoups are allowed in our website that makes it suitable for every age of users.In Bangladesh,  We are now  in leading position of selling which shows every months nor  than 10,000 headphones are being sold. 
 
-Welcome to BD Headphones Hub,Thank you.
+
 
 
--- index.js
+++ index.js
@@ -9,28 +9,25 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middlewere
+
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwk7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwknw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 async function run() {
 	try {
 		await client.connect();
-		const database = client.db('bdHeadphonesHub');
+		const database = client.db('headphones');
 		const productsCollection = database.collection('products');
 		const productOrderItems = database.collection('orderItems');
 		const usersCollection = database.collection('users');
 		const usersReview = database.collection('review');
 
-
-		/*---------------------------------------
-					   GET POST STARTED
-		 -------------------------------------- */
 
 
 		// PURE UPDATE FOR ADMIN
@@ -183,9 +180,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-	res.json('bd-headphone-hub')
+	res.json('headphones')
 });
 
 app.listen(port, () => {
-	console.log('Running BD Headphone Hub', port);
+	console.log('Running New Headphone Technica', port);
 })
